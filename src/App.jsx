8553cd55fd5a5803b42af66ee48e044731e72c10
raineldn <raineldn@gmail.com>
2019-06-26 13:47:22 +0100

    Added different hexagonal SVG

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import Button from "./components/Button.jsx";
 import axios from "axios";
 import styled from 'styled-components';
-import { HexGrid, Layout, Hexagon, Text, Pattern, Path, Hex } from 'react-hexgrid';
-import './App.css';
 import Form from "./components/Form.jsx";
+import Hexagon from 'react-hexagon';
+import './App.css';
 
 // Will clean up the code and convert it to styled-components
 
@@ -56,48 +56,31 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">GitHive</h1>
         </header>
-        <p className="App-intro">Watch this space... (P.S. These are NOT the project colours, lol)</p>
+        <p className="App-intro">Watch this space...</p>
         <Form
-          formData={this.state.formData}
-          handleUserFormSubmit={this.handleUserFormSubmit}
-          handleFormChange={this.handleFormChange}
-        />
-
-        <div className="HexagonGrid">
-          <HexGrid className="hexgrid">
-            <Layout size={{ x: 10, y: 10}} flat={true} spacing={1.1} height={400} width={400}>
-              <img image="true" src={this.state.profile_image} />
-              <Hexagon q={0} r={0} s={0}>  
-                <Text>Username:{this.state.gitun}</Text>
-              </Hexagon>
-              <Hexagon q={0} r={1} s={0}> 
-                <Text>Location: {this.state.location} </Text>
-              </Hexagon>
-              <Hexagon q={1} r={-1} s={0}> 
-                <Text>Bio: {this.state.bio} </Text>
-              </Hexagon>
-              <Hexagon q={1} r={0} s={0}>
-                <Text>Followers: {this.state.followers} </Text>
-              </Hexagon>
-              <Hexagon q={1} r={1} s={0}>
-                <Text>More Further Down</Text>
-              </Hexagon>
-              <Hexagon q={2} r={-1} s={0}>
-                <Text>Following: {this.state.following} </Text>
-              </Hexagon>
-              <Hexagon q={2} r={0} s={0}> 
-                <Text>No. of Repos: {this.state.public_repos} </Text>
-              </Hexagon>
-              <Hexagon q={3} r={-1} s={0}> 
-                <Text>Hireable: {this.state.hireable} </Text>
-              </Hexagon>
-
-              <b>Information:</b>
-              <pre>{this.state.info}</pre>
-            </Layout>
-          </HexGrid>
-        </div>
-      </div>
+            formData={this.state.formData}
+            handleUserFormSubmit={this.handleUserFormSubmit}
+            handleFormChange={this.handleFormChange}
+          />
+        <div className="cellContainer">
+                 <Hexagon
+                        style={{stroke: '#000'}}
+                        className="hex"
+                        backgroundImage={this.state.profile_image}
+                        src={this.state.profile_image}
+                        flatTop
+                        />
+                  <g><Hexagon className="cell, username" flatTop><text x="180" y="250">Username:{this.state.gitun}</text></Hexagon></g>
+                  <p>Location: {this.state.location} </p>
+                  <p>Bio: {this.state.bio} </p>
+                  <p>Followers: {this.state.followers} </p>
+                  <p>Following: {this.state.following} </p>
+                  <p>No. of Repos: {this.state.public_repos} </p>
+                  <p>Hireable: {this.state.hireable} </p>
+                  <b>Information:</b>
+                  <pre>{this.state.info}</pre>
+          </div>
+          </div>
     );
   }
 }
